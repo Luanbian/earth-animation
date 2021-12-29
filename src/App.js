d100f557ab3acc70683d34b4dc573ec6bc4e15c8
Luan de Almeida <luanbianchine1@gmail.com>
2021-12-29 18:53:51 -0300

    iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import styled from 'styled-components';
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+function App() {
+  return (
+    <CanvasContainer>Hello three.js</CanvasContainer>
+  );
+}
+
+export default App;
